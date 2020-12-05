91cb10c1c5ab0c3d9a6975dc0ca1d8573fc17540
Muaz Wazir <muazwazir@gmail.com>
2020-12-06 02:25:36 +0800

    helper function

--- Parser.java
+++ Parser.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 
 class Parser {
 	public final static Scanner in = new Scanner(System.in);
-	static String token;
+	static String[] token;
+	static int index = 0;
 
+	// Constants for operators
 	static final char ADD = '+';
 	static final char SUB = '-';
 	static final char MUL = '*';
@@ -12,7 +14,7 @@ class Parser {
 	static final char MOD = '%';
 	static final char ASSIGN = '=';
 
-
+	// Constant for identifier
 	static final String LET = "let";
 	static final String CONST = "const";
 	static final String VAR = "var";
@@ -24,16 +26,23 @@ class Parser {
 		in.useDelimiter("\n");
 		System.out.println("Enter String to validate");
 		String stmt = in.next();
-		
 
-		String[] token = stmt.split("\\s");
-		for (String w: token) {
-			System.out.println(w);
+		token = stmt.split("\\s");
+		for (int i = 0; i < token.length; i++) {
+			try {
+				System.out.println(getNextToken());
+			} catch(Exception e) {
+				System.out.println(e);
+			}
 		}
-
 	}
 
 	public static String getToken() {
-		return token = in.next();
+		return token[index];
+	}
+
+	public static String getNextToken() throws Exception {
+		if (index > token.length) throw new Exception("Array out of bound");
+		return token[index++];
 	}
 }
